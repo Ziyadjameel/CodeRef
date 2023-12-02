cad3184de2cd7705d23e9ae020088fdf72f8daec
Almahri90 <128807526+Almahri90@users.noreply.github.com>
2023-12-02 21:26:33 +0300

    Mohammed- Improved indentation and formatting

--- coderefactor.java
+++ coderefactor.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class SensorDataProcessor {
+
     private double[][][] data;
     private double[][] limit;
 
@@ -27,16 +28,19 @@ public class SensorDataProcessor {
             for (int i = 0; i < data.length; i++) {
                 for (int j = 0; j < data[0].length; j++) {
                     for (int k = 0; k < data[0][0].length; k++) {
-                        processedData[i][j][k] = data[i][j][k] / divisor - Math.pow(limit[i][j], 2.0);
+                        double normalizedValue = (data[i][j][k] / divisor) - Math.pow(limit[i][j], 2.0);
 
+                        // Check for anomalies
                         if (calculateAverage(processedData[i][j]) > 10 && calculateAverage(processedData[i][j]) < 50) {
                             break;
-                        } else if (Math.max(data[i][j][k], processedData[i][j][k]) > data[i][j][k]) {
+                        } else if (Math.max(data[i][j][k], normalizedValue) > data[i][j][k]) {
                             break;
-                        } else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(processedData[i][j][k]), 3) &&
-                                calculateAverage(data[i][j]) < processedData[i][j][k] && (i + 1) * (j + 1) > 0) {
-                            processedData[i][j][k] *= 2;
+                        } else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(normalizedValue), 3) &&
+                                calculateAverage(data[i][j]) < normalizedValue && (i + 1) * (j + 1) > 0) {
+                            normalizedValue *= 2;
                         }
+
+                        processedData[i][j][k] = normalizedValue;
                     }
                 }
             }
@@ -45,6 +49,7 @@ public class SensorDataProcessor {
                 for (double[] column : row) {
                     writer.write(Arrays.toString(column) + "\t");
                 }
+                writer.write("\n");
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
